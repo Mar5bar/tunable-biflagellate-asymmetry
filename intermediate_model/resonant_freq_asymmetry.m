6305bd22534e00ac3e6f041a540138c3bcafe36b
Benjamin Walker <7798410+Mar5bar@users.noreply.github.com>
2026-04-27 12:07:02 +0100

    resonant_freq_asymmetry example simplified to just one frequency

--- intermediate_model/resonant_freq_asymmetry.m
+++ intermediate_model/resonant_freq_asymmetry.m
@@ -9,11 +9,11 @@ alpha = 1;
 beta = 0.05;
 kL = 1;
 
-kRs = linspace(1.1,3.5,1e3);
+kRs = linspace(1.1,3,1e3);
 vals = 0*kRs;
 approxVals = 0*kRs;
 
-K = @(t,k) sin(k*t) + sin(2*k*t);
+K = @(t,k) sin(k*t);
 
 for i = 1 : length(kRs)
 	kR = kRs(i);
@@ -31,9 +31,9 @@ box on
 xlabel('$k_R/k_L$')
 ylabel('$\int\cos{\theta(t)} \, \mathrm{d}t$')
 
-inset = axes('Position',[0.471223021582734 0.1994 0.366906474820144 0.238297872340426]);
+inset = axes('Position',[0.471223021582734 0.22 0.366906474820144 0.238297872340426]);
 plot(kRs, vals - approxVals, 'Color', 'black')
-set(inset, 'XTick', [2,3])
+set(inset, 'XTick', [2])
 set(inset, 'YTick', [])
 xlim([min(kRs),max(kRs)])
 box on
