75c76917e13bd520baa40a118e60148fcc783e6d
Benjamin Walker <7798410+Mar5bar@users.noreply.github.com.>
2026-04-29 10:35:52 +0100

    alpha,beta -> gamma, delta in resonant freq asym fig

--- intermediate_model/resonant_freq_asymmetry.m
+++ intermediate_model/resonant_freq_asymmetry.m
@@ -3,10 +3,11 @@ clear all
 
 figure('Position',[812 334 556 470])
 hold on
+fontsize = 24;
 
 ts = linspace(0,30*pi,1e5);
-alpha = 1;
-beta = 0.05;
+gamma = 1;
+delta = 0.05;
 kL = 1;
 
 kRs = linspace(1.1,3,1e3);
@@ -17,19 +18,20 @@ K = @(t,k) sin(k*t);
 
 for i = 1 : length(kRs)
 	kR = kRs(i);
-	vals(i) = trapz(ts, cos(alpha*(kR - kL)*ts + beta*(K(ts,kR) - K(ts,kL))));
-	approxVals(i) = trapz(ts, cos(alpha*(kR - kL)*ts));
+	vals(i) = trapz(ts, cos(gamma*(kR - kL)*ts + delta*(K(ts,kR) - K(ts,kL))));
+	approxVals(i) = trapz(ts, cos(gamma*(kR - kL)*ts));
 end
 
 plot(kRs, vals, 'LineWidth', 2, 'Color', 'black')
 plot(kRs, approxVals, 'LineWidth', 1, 'Color', 0.5*[1,1,1], 'LineStyle','-')
-legend({'$\beta \ll \alpha$', '$\beta = 0$'}, 'Interpreter', 'latex')
+legend({'$\delta \ll \gamma$', '$\delta = 0$'}, 'Interpreter', 'latex')
 xlim([min(kRs),max(kRs)])
 ylim([-9,7])
 axis square
 box on
 xlabel('$k_R/k_L$')
 ylabel('$\int\cos{\theta(t)} \, \mathrm{d}t$')
+set(gca,'FontSize', fontsize)
 
 inset = axes('Position',[0.471223021582734 0.22 0.366906474820144 0.238297872340426]);
 plot(kRs, vals - approxVals, 'Color', 'black')
@@ -39,5 +41,6 @@ xlim([min(kRs),max(kRs)])
 box on
 ylabel('Error')
 yline(0,'Color','black')
+set(gca,'FontSize', fontsize)
 
 exportgraphics(gcf, 'resonant_freq_asymmetry.pdf')
